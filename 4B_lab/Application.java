53426c60088251f55371ea12d73a0d52055d3408
K JOSHITHA SATYA <joshitha.koyyana0201@gmail.com>
2024-08-13 10:33:40 +0530

    Update Application.java

--- 4B_lab/Application.java
+++ 4B_lab/Application.java
@@ -2,22 +2,22 @@ package FourB;
 
 public class Application {
 	    public static void main(String[] args) {
-	        // Set up the handlers
+	       
 	        Handler lowHandler = new LowSeverityHandler();
 	        Handler mediumHandler = new MediumSeverityHandler();
 	        Handler highHandler = new HighSeverityHandler();
 
-	        // Set the chain of responsibility
+	        
 	        lowHandler.setNextHandler(mediumHandler);
 	        mediumHandler.setNextHandler(highHandler);
 
-	        // Create logger and actions
+	        
 	        LoggerManager logger = new LoggerManager();
 	        logger.addAction(new LogAction(lowHandler, Severity.LOW));
 	        logger.addAction(new LogAction(mediumHandler, Severity.MEDIUM));
 	        logger.addAction(new LogAction(highHandler, Severity.HIGH));
 
-	        // Execute the logging actions
+	        
 	        logger.executeActions();
 	    }
 	}
