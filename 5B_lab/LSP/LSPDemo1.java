c2c6f91e96b9b7f192e7310289e0a1ad5d6cae38
K JOSHITHA SATYA <joshitha.koyyana0201@gmail.com>
2024-08-13 13:25:34 +0530

    Add files via upload

---
+++ 5B_lab/LSP/LSPDemo1.java
@@ -0,0 +1,12 @@
+package lsp;
+
+	public class LSPDemo1 {
+	    public static void main(String[] args) {
+	        Bird eagle = new Bird();
+	        eagle.fly();
+
+	        Bird penguin = new Penguin();
+	        penguin.fly();
+	    }
+	}
+
---
+++ 5B_lab/LSP/Penguin.java
@@ -0,0 +1,8 @@
+package lsp;
+
+	public class Penguin extends Bird {
+	    @Override
+	    public void fly() {
+	        System.out.println("Penguin cannot fly.");
+	    }
+	}
