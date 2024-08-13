79a560c1252f5ea4c836add583888ae4cada65a1
K JOSHITHA SATYA <joshitha.koyyana0201@gmail.com>
2024-08-13 10:32:19 +0530

    Update AuctionSystem.java

--- 4A_lab/AuctionSystem.java
+++ 4A_lab/AuctionSystem.java
@@ -4,20 +4,19 @@ package FourA;
 	    public static void main(String[] args) {
 	        AuctionEvent auctionEvent = new AuctionEvent();
 
-	        // Create participants
+	        
 	        Participant participant1 = new Participant("Joshi");
 	        Participant participant2 = new Participant("Deepu");
 
-	        // Register participants for auction notifications
 	        auctionEvent.addListener(participant1);
 	        auctionEvent.addListener(participant2);
 
-	        // Auction events
+	        
 	        auctionEvent.newItem("Vintage Watch");
 	        auctionEvent.beginAuction();
 	        auctionEvent.concludeAuction();
 
-	        // Unsubscribe a participant
+	       
 	        auctionEvent.removeListener(participant1);
 
 	        // Another auction event after unsubscription
